4539d49f53de0eebb3cc8f54c1b9238819ed929e
AngelDevHub <rvangel44gc@gmail.com>
2026-03-23 20:35:20 -0600

    fix: routing, assets and payment-methods api url

--- src/app/core/services/payment-method.service.ts
+++ src/app/core/services/payment-method.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
 import { PaymentMethod } from '../models';
 
 @Injectable({
@@ -8,7 +9,7 @@ import { PaymentMethod } from '../models';
 })
 export class PaymentMethodService {
   private http = inject(HttpClient);
-  private apiUrl = '/api/v1/payment-methods';
+  private apiUrl = `${environment.apiUrl}/payment-methods`;
 
   getAll(): Observable<PaymentMethod[]> {
     return this.http.get<PaymentMethod[]>(this.apiUrl);
